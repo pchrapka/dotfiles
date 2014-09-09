260939faaba710f7c8197c1eb589f7a08ec3a5b3
Phil Chrapka <pchrapka@gmail.com>
2014-09-09 10:57:28 -0400

    fixing symlinks for cygwin

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,10 +6,28 @@
 # Run this script from the home directory
 ############################
 
-ln -sb projects/dotfiles/.screenrc .
-ln -sb projects/dotfiles/.bash_profile .
-ln -sb projects/dotfiles/.bashrc .
-ln -sb projects/dotfiles/.bashrc_custom .
-ln -sb projects/dotfiles/.emacs .
-mv .emacs.d .emacs.d~
-ln -s projects/dotfiles/.emacs.d .
+case "$(uname)" in
+   CYGWIN*) cygwin=1 ;;
+   *)       cygwin=0 ;;
+esac
+
+if [ $cygwin -eq 1 ]; then
+    # Cygwin
+    dir=/cygdrive/c/Users/Phil/My\ Projects/dotfiles
+    echo $dir
+    ln -sb "${dir}"/.screenrc $HOME
+	ln -sb "${dir}"/.bash_profile $HOME
+	ln -sb "${dir}"/.bashrc $HOME
+	ln -sb "${dir}"/.bashrc_custom $HOME
+	ln -sb "${dir}"/.emacs $HOME
+	mv $HOME/.emacs.d $HOME/.emacs.d~
+	ln -s "${dir}"/.emacs.d $HOME
+else
+	ln -sb projects/dotfiles/.screenrc .
+	ln -sb projects/dotfiles/.bash_profile .
+	ln -sb projects/dotfiles/.bashrc .
+	ln -sb projects/dotfiles/.bashrc_custom .
+	ln -sb projects/dotfiles/.emacs .
+	mv .emacs.d .emacs.d~
+	ln -s projects/dotfiles/.emacs.d .
+fi
